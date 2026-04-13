9f2ad3d4fbdab74461a9e6f820f9907089c613fc
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2026-04-13 10:47:25 -0400

    Revert "Feature: adding expenses category types"

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -21,14 +21,14 @@ router.get("/", async (req: Request, res: Response) => {
 
 // Add a new expense
 router.post("/", async (req: Request, res: Response) => {
-  const { description, amount, paidById, category } = req.body;
+  const { description, amount, paidById } = req.body;
   try {
     const expense = await prisma.expense.create({
-      data: { description, amount, paidById, category },
+      data: { description, amount, paidById },
       include: { paidBy: true },
     });
     logger.info(
-      `Created expense: ${category} for $${amount} by user ${paidById}`
+      `Created expense: ${description} for $${amount} by user ${paidById}`
     );
     return res.status(201).json(expense);
   } catch (error) {

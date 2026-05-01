9fcb839a43ab1bd691501de87ce5cd8d015a9a0c
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2026-04-30 20:28:51 -0400

    updating globalsetup

--- src/__tests__/globalSetup.ts
+++ src/__tests__/globalSetup.ts
@@ -1,4 +1,7 @@
 import { execSync } from 'child_process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default async function globalSetup() {
   process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
